f287f6d248498a0ddf188effd5e19dd80f5d99a1
Boothu <declanryan.db@gmail.com>
2025-07-29 18:57:37 +0100

    Add DELETE endpoint to ExpenseController for removing expenses

--- budgettracker/src/main/java/com/boothu/budgettracker/controller/ExpenseController.java
+++ budgettracker/src/main/java/com/boothu/budgettracker/controller/ExpenseController.java
@@ -3,7 +3,9 @@ package com.boothu.budgettracker.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +33,12 @@ public class ExpenseController {
     @GetMapping
     public List<Expense> getAllExpenses() {
         return expenseRepository.findAll();
+    }
+
+    // Endpoint: DELETE /expenses/{id}
+    @DeleteMapping("/{id}") // Maps this method to a DELETE request
+    // @PathVariable extracts {id} from the URL
+    public void deleteExpense(@PathVariable Long id) {
+        expenseRepository.deleteById(id);
     }
 }
