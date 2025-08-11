0ca85e1eac798b0d2309f7172ef749c16789ef18
Boothu <declanryan.db@gmail.com>
2025-08-11 16:47:13 +0100

    Add documentation via swagger

--- budgettracker/src/main/java/com/boothu/budgettracker/BudgettrackerApplication.java
+++ budgettracker/src/main/java/com/boothu/budgettracker/BudgettrackerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BudgettrackerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BudgettrackerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BudgettrackerApplication.class, args);
+    }
 
 }
